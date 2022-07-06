6de99faa7e08d14164d105ec3a69e09d6765be0e
thatretrodev <thatretrodev@outlook.com>
2022-07-06 19:40:07 -0400

    More logging & Reactions

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ generator = pipeline("text-generation", model="gpt2")
 
 load_dotenv()
 
+reaction_empty_message = "😐"
+
 class BotClient(disnake.Client):
 	async def on_ready(self):
 		print(f"Logged in as {self.user} (ID: {self.user.id})")
@@ -21,21 +23,35 @@ class BotClient(disnake.Client):
 		if message.author.id == self.user.id:
 			return
 
-		if message.content.lower().endswith("?") or message.content.lower().startswith("when ") or message.content.lower().startswith("can ") or message.content.lower().startswith("will ") or message.content.lower().startswith("who ") or message.content.lower().startswith("have ") or message.content.lower().startswith("how ") or message.content.lower().startswith("what ") or message.content.lower().endswith("when"):
+		if message.content.lower().endswith("?") or message.content.lower().startswith("why ") or message.content.lower().startswith("does ") or message.content.lower().startswith("when ") or message.content.lower().startswith("can ") or message.content.lower().startswith("will ") or message.content.lower().startswith("who ") or message.content.lower().startswith("have ") or message.content.lower().startswith("how ") or message.content.lower().startswith("what ") or message.content.lower().endswith("when"):
 			print(f"{message.author} ({message.guild.name}): {message.content}")
-			set_seed(random.randint(0, 999999999))
+			
+			gpt2_seed = random.randint(0, 999999999)
+			gpt2_max_length = 100
+
+			print("GPT2 seed:", gpt2_seed)
+
+			set_seed(gpt2_seed)
 
 			author_name = message.author.name
 			question_text = message.content
 
 			prompt = f"Answer {author_name}'s Question: \"{question_text}\""
 
+			if len(prompt) > gpt2_max_length:
+				await message.add_reaction(reaction_empty_message)
+				return
+
 			await message.channel.trigger_typing()
 
-			output = generator(prompt, max_length=100, num_return_sequences=1)[0]["generated_text"].replace(prompt, "")
+			output = generator(prompt, max_length=gpt2_max_length, num_return_sequences=1)[0]["generated_text"].replace(prompt, "").strip()
 
-			await message.reply(output)
-			return
+			if output == "":
+				await message.add_reaction(reaction_empty_message)
+				return
+			else:
+				await message.reply(output)
+				return
 
 client = BotClient()
 client.run(os.environ["BOT_TOKEN"])
